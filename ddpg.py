a546640baa1bd5c1638dd82af5618deb6dbb4b1a
Chu Hung Cheng <chccheng@ucdavis.edu>
2018-05-02 21:39:06 -0700

    Pre Actor

--- ddpg.py
+++ ddpg.py
@@ -2,6 +2,10 @@
 import numpy as np
 import tensorflow as tf
 
+#### Global Variables
+TAU = 0.01
+
+
 #### Actor
 class Actor():
     # __init__
@@ -12,19 +16,83 @@ class Actor():
         train_op => gradient descent
         policy_grads => gradient of policy/action over target_params
     '''
-    def __init__(self, sess, action_dim, action_bound, learning_rate, replacement):
-        
+    def __init__(self, sess, action_dim, learning_rate):
+        self.sess = sess
+        self.a_dim = action_dim
+        self.lr = learning_rate
+
+        with tf.variable_scope('Actor'):
+            # action network
+            self.a = build_actor(S, 'Actor/actor_network', trainable = True)
+            # target action network
+            self.target_a = build_actor(S_, 'Actor/target_network', trainable = False)
+
+        # randomly initialize the parameters for neural network
+        self.a_params = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope = 'Actor/actor_network')
+        self.target_a_params = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope = 'Actor/target_network')
+
+        # slowly update target_params
+        self.soft_update = [
+            tf.assign(t, (1 - TAU) * t + TAU * e )
+            for (t,e) in zip(self.target_a_params, self.a_params)
+        ]
+
+        # gradient of policy over target params
+        self.policy_gradient = tf.gradients(ys = self.a , xs = self.target_a_params, grad_ys = q_grads)
+
+        # train_op
+        self.train_op = tf.train.GradientDescentOptimizer(self.lr).minimize(-self.policy_gradient, var_list = self.a_params)
 
     # _build_net
     # Build the neural network. 
     # 2 hidden layer, 30 nodes each
     def build_actor(self, s, scope, trainable):
+        with tf.variable_scope(scope):
+            init_w = tf.random_normal_initializer(0, 0.1)
+            init_b = tf.constant_initializer(0.1)
+            node_count = 30
+
+            with tf.variable_scope('l1'):
+                l1 = tf.layers.dense(
+                    s,
+                    node_count,
+                    activation = tf.nn.relu,
+                    kernel_initializer= init_w,
+                    bias_initializer = init_b,
+                    name = 'l1',
+                    trainable = trainable
+                )
+            with tf.variable_scope('l2'):
+                l2 = tf.layers.dense (
+                    l1,
+                    node_count,
+                    activation=tf.nn.relu,
+                    kernel_initializer=init_w,
+                    bias_initializer=init_b,
+                    name='l2',
+                    trainable = trainable
+                )
+
+            with tf.variable_scope('output_layer'):
+                output_layer = tf.layers.dense(
+                    l2,
+                    self.a_dim,
+                    activation = tf.nn.relu,
+                    kernel_initializer = init_w,
+                    bias_initializer = init_b,
+                    name = 'output_layer',
+                    trainable = trainable
+                )
+
+        return output_layer
 
     # learn
     def learn(self, s):
+        self.sess.run(self.train_op, feed_dict = {S: s})
     
     # choose action
     def choose_action(self, s):
+        self.sess.run(self.a, feed_dict = {S: s})
 
 #### Critic
 
